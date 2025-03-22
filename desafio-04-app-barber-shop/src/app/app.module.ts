0b6c46b1d09e9c50a75cb31a1316ec3d45bac8eb
Alcides Neto <alcidesneto40@gmail.com>
2025-03-22 14:54:35 -0300

    Criação de componentes e serviços

--- desafio-04-app-barber-shop/src/app/app.module.ts
+++ desafio-04-app-barber-shop/src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewClientComponent } from './clients/new-client/new-client.component';
+import { ListClientsComponent } from './clients/list-clients/list-clients.component';
+import { EditClientComponent } from './clients/edit-client/edit-client.component';
+import { ClientFormComponent } from './clients/components/client-form/client-form.component';
+import { ClientTableComponent } from './clients/components/client-table/client-table.component';
+import { SchedulesMonthComponent } from './schedules/schedules-month/schedules-month.component';
+import { ScheduleCalendarComponent } from './schedules/components/schedule-calendar/schedule-calendar.component';
+import { CardHeaderComponent } from './commons/components/card-header/card-header.component';
+import { MenuBarComponent } from './commons/components/menu-bar/menu-bar.component';
+import { DialogComponent } from './commons/components/dialog/dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewClientComponent,
+    ListClientsComponent,
+    EditClientComponent,
+    ClientFormComponent,
+    ClientTableComponent,
+    SchedulesMonthComponent,
+    ScheduleCalendarComponent,
+    CardHeaderComponent,
+    MenuBarComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ desafio-04-app-barber-shop/src/app/schedules/schedules-month/schedules-month.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedules-month',
+  templateUrl: './schedules-month.component.html',
+  styleUrls: ['./schedules-month.component.scss']
+})
+export class SchedulesMonthComponent {
+
+}
