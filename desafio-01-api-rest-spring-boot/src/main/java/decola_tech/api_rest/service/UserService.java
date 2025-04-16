2434ae2715cce45c38aecfe026819c2532841568
Alcides Neto <alcidesneto40@gmail.com>
2025-04-16 02:45:40 -0300

    Modificação do UserService para usar Optional

--- desafio-01-api-rest-spring-boot/src/main/java/decola_tech/api_rest/service/UserService.java
+++ desafio-01-api-rest-spring-boot/src/main/java/decola_tech/api_rest/service/UserService.java
@@ -1,11 +1,13 @@
 package decola_tech.api_rest.service;
 
+import java.util.Optional;
+
 import decola_tech.api_rest.domain.models.User;
 
 public interface UserService {
     
     
-    User findById(Long id);
+    Optional<User> findById(Long id);
 
     User create(User userToCreate);
 }
